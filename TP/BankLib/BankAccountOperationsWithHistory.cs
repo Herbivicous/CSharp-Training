d56ed57e3649aebd1f5715d9b387a5f40edb8229
Bastien <user5@eni1021.onmicrosoft.com>
2021-10-21 16:08:36 +0200

    changes in names

--- TP/BankLib/BankAccountOperationsWithHistory.cs
+++ TP/BankLib/BankAccountOperationsWithHistory.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TP.BankLib.model;
-using TP.Client;
-using TP.Utils;
 
 namespace TP.BankLib
 {
--- TP/BankLib/Model/BankAccountInfo.cs
+++ TP/BankLib/Model/BankAccountInfo.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TP.Client;
-using TP.Utils;
 
 namespace TP.BankLib.model
 {
